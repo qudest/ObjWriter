44470b1596327e93a26fb9253fcc5998cc9ed57c
nagornyy_a_s <nagornyi.arsenii@mail.ru>
2023-11-29 22:30:49 +0300

    Implement methods for writing

--- src/main/java/com/cgvsu/Main.java
+++ src/main/java/com/cgvsu/Main.java
@@ -2,6 +2,7 @@ package com.cgvsu;
 
 import com.cgvsu.model.Model;
 import com.cgvsu.objreader.ObjReader;
+import com.cgvsu.objwriter.ObjWriter;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -11,7 +12,7 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
 
-        Path fileName = Path.of("3DModels/Faceform/WrapHead.obj");
+        Path fileName = Path.of("3DModels/Faceform/AlexWithTexture/AlexNeutralWrapped.obj");
         String fileContent = Files.readString(fileName);
 
         System.out.println("Loading model ...");
@@ -21,5 +22,20 @@ public class Main {
         System.out.println("Texture vertices: " + model.textureVertices.size());
         System.out.println("Normals: " + model.normals.size());
         System.out.println("Polygons: " + model.polygons.size());
+
+        String file = "file.obj";
+        ObjWriter.write(file, model);
+
+
+        Path fileName1 = Path.of("file.obj");
+        String fileContent1 = Files.readString(fileName1);
+
+        System.out.println("Loading model ...");
+        Model model1 = ObjReader.read(fileContent);
+
+        System.out.println("Vertices: " + model1.vertices.size());
+        System.out.println("Texture vertices: " + model1.textureVertices.size());
+        System.out.println("Normals: " + model1.normals.size());
+        System.out.println("Polygons: " + model1.polygons.size());
     }
 }
--- src/main/java/com/cgvsu/objwriter/ObjWriter.java
+++ src/main/java/com/cgvsu/objwriter/ObjWriter.java
@@ -6,7 +6,6 @@ import com.cgvsu.model.Model;
 import com.cgvsu.model.Polygon;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
@@ -20,11 +19,11 @@ public class ObjWriter {
         File file = new File(fileName);
 
         try {
-            if (!file.exists()) {
-                file.createNewFile();
+            if (!file.exists() && !file.createNewFile()) {
+                throw new ObjWriterException("Unable to create the file: " + fileName);
             }
         } catch (IOException e) {
-            System.out.println();
+            throw new ObjWriterException(e.getMessage());
         }
 
         try (PrintWriter printWriter = new PrintWriter(file)) {
@@ -33,26 +32,53 @@ public class ObjWriter {
             writeNormals(printWriter, model.normals);
             writePolygons(printWriter, model.polygons);
         } catch (IOException e) {
-            throw new ObjWriterException(e.getMessage());
+            throw new ObjWriterException("Error writing to file: " + fileName);
         }
     }
-    protected static void writeVertices(PrintWriter pw, ArrayList<Vector3f> vertices) {
-        pw.println(OBJ_VERTEX_TOKEN);
+    protected static void writeVertices(PrintWriter pw, ArrayList<Vector3f> vertices) throws IOException {
+        for (Vector3f vertex: vertices) {
+            pw.println(OBJ_VERTEX_TOKEN + " " + vertex.x + " " + vertex.y + " " + vertex.z);
+        }
         pw.println();
     }
 
-    protected static void writeTextureVertices(PrintWriter pw, ArrayList<Vector2f> textureVertices) {
-        pw.println(OBJ_TEXTURE_TOKEN);
+    protected static void writeTextureVertices(PrintWriter pw, ArrayList<Vector2f> textureVertices) throws IOException {
+        for (Vector2f vertex: textureVertices) {
+            pw.println(OBJ_TEXTURE_TOKEN + " " + vertex.x + " " + vertex.y);
+        }
         pw.println();
     }
 
-    protected static void writeNormals(PrintWriter pw, ArrayList<Vector3f> normals) {
-        pw.println(OBJ_NORMAL_TOKEN);
+    protected static void writeNormals(PrintWriter pw, ArrayList<Vector3f> normals) throws IOException {
+        for (Vector3f normal: normals) {
+            pw.println(OBJ_NORMAL_TOKEN + " " + normal.x + " " + normal.y + " " + normal.z);
+        }
         pw.println();
     }
 
-    protected static void writePolygons(PrintWriter pw, ArrayList<Polygon> polygons) {
-        pw.println(OBJ_FACE_TOKEN);
-        pw.println();
+    protected static void writePolygons(PrintWriter pw, ArrayList<Polygon> polygons) throws IOException {
+        for (Polygon polygon : polygons) {
+            ArrayList<Integer> vertexIndices = polygon.getVertexIndices();
+            ArrayList<Integer> textureVertexIndices = polygon.getTextureVertexIndices();
+            ArrayList<Integer> normalIndices = polygon.getNormalIndices();
+
+            pw.print(OBJ_FACE_TOKEN + " ");
+            for (int i = 0; i < vertexIndices.size(); i++) {
+                if (!textureVertexIndices.isEmpty()) {
+                    pw.print((vertexIndices.get(i) + 1) + "/" + (textureVertexIndices.get(i) + 1));
+                } else {
+                    pw.print(vertexIndices.get(i) + 1);
+                }
+                if (!normalIndices.isEmpty()) {
+                    if (textureVertexIndices.isEmpty()) {
+                        pw.print("/");
+                    } else {
+                        pw.print("/" + (normalIndices.get(i) + 1));
+                    }
+                }
+                pw.print(" ");
+            }
+            pw.println();
+        }
     }
 }
